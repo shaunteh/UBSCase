1b7c476df741044bf6055fb835159f87ca4ff106
Kai Xuan <ngkx174@gmail.com>
2015-06-25 20:40:17 +0800

    Added styling

--- UBSCase/web/js/app.js
+++ UBSCase/web/js/app.js
@@ -2,34 +2,44 @@ var app = angular.module("UBSCase", ['ui.router', 'ngCookies']);
 
 
 // For Application Routing
-app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+app.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
     $urlRouterProvider.otherwise("/");
     $locationProvider.html5Mode(true);
     console.log("Here");
     $stateProvider
-            .state('/dashboard', {
-                url: "/UBSCase/dashboard",
+            .state('/', {
+                url: '/',
                 controller: 'DashboardController',
                 templateUrl: 'js/views/dashboard.html',
                 data: {
-                    requireLogin: true
+                    requireLogin: false
                 }
             })
-            .state('/', {
-                url: '/',
-                controller: "LoginController",
-                templateUrl: "js/views/login.html",
+            /*
+             .state('/', {
+             url: '/',
+             controller: 'LoginController',
+             templateUrl: 'js/views/login.html',
+             data: {
+             requireLogin: false
+             }
+             })
+             */
+            .state('/portfolio', {
+                url: '/UBSCase/portfolio',
+                controller: "DashboardController",
+                templateUrl: "js/views/portfolio.html",
                 data: {
                     requireLogin: false
                 }
             });
 
+
 });
 
-
 // For managing state changes
-app.run(function($rootScope, $location, $cookieStore) {
-    $rootScope.$on('$stateChangeStart', function(event, toState, toParams) {
+app.run(function ($rootScope, $location, $cookieStore) {
+    $rootScope.$on('$stateChangeStart', function (event, toState, toParams) {
         var requireLogin = toState.data.requireLogin;
         var isRefreshed = typeof $rootScope.currentUser === 'undefined';
         console.log("logging cookie!" + $cookieStore.get("user"));
@@ -55,15 +65,16 @@ app.run(function($rootScope, $location, $cookieStore) {
 });
 
 // for CSRF
-app.config(function($httpProvider) {
+app.config(function ($httpProvider) {
     //fancy random token
     function b(a) {
         return a ? (a ^ Math.random() * 16 >> a / 4).toString(16) : ([1e16] + 1e16).replace(/[01]/g, b)
-    };
+    }
+    ;
 
-    $httpProvider.interceptors.push(function() {
+    $httpProvider.interceptors.push(function () {
         return {
-            'request': function(config) {
+            'request': function (config) {
                 // put a new random secret into our CSRF-TOKEN Cookie after each response
                 document.cookie = 'CSRF-TOKEN=' + b();
                 return config;
@@ -73,19 +84,19 @@ app.config(function($httpProvider) {
 });
 
 app.constant('USER_ROLES', {
-  all: '*',
-  admin: 'admin',
-  editor: 'editor',
-  guest: 'guest'
+    all: '*',
+    admin: 'admin',
+    editor: 'editor',
+    guest: 'guest'
 });
 
 app.constant('AUTH_EVENTS', {
-  loginSuccess: 'auth-login-success',
-  loginFailed: 'auth-login-failed',
-  logoutSuccess: 'auth-logout-success',
-  sessionTimeout: 'auth-session-timeout',
-  notAuthenticated: 'auth-not-authenticated',
-  notAuthorized: 'auth-not-authorized'
+    loginSuccess: 'auth-login-success',
+    loginFailed: 'auth-login-failed',
+    logoutSuccess: 'auth-logout-success',
+    sessionTimeout: 'auth-session-timeout',
+    notAuthenticated: 'auth-not-authenticated',
+    notAuthorized: 'auth-not-authorized'
 });
 
 
